7c7bac3740acf68618d0187023d4ae7e90f3a4b3
ganeshkale07 <ggkale07@gmail.com>
2021-12-30 19:25:07 +0530

    video 56

---
+++ src/component/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            these is about 
+        </div>
+    )
+}
+
+export default About
---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            these is home
+        </div>
+    )
+}
+
+export default Home
